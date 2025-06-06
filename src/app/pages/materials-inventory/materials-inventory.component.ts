6b53bdeecfc3a665984bc759ee041325ee05ac17
Tran Tuan Anh <trananh0604@gmail.com>
2025-06-06 10:32:50 +0700

    0606

--- src/app/pages/materials-inventory/materials-inventory.component.ts
+++ src/app/pages/materials-inventory/materials-inventory.component.ts
@@ -11,7 +11,7 @@ interface Inventory {
 @Component({
   selector: 'app-materials-inventory',
   templateUrl: './materials-inventory.component.html',
-  styleUrls: ['./materials-inventory.component.css']
+  styleUrls: ['./materials-inventory.component.scss']
 })
 export class MaterialsInventoryComponent implements OnInit {
   inventoryList: Inventory[] = [];
