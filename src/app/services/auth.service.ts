4db3411c340be26ec2010b750f351563400b9016
Tran Tuan Anh <trananh0604@gmail.com>
2025-06-17 10:40:22 +0700

    n

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ declare const gapi: any;
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private clientId = '286089206981-k5p1g4c91jqdlck27582t57g9mstj58d.apps.googleusercontent.com';
-  private scope = 'https://www.googleapis.com/auth/spreadsheets';
+  private scope = 'https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/drive.readonly';
 
   public token: string = '';
   public isSignedIn: boolean = false;
@@ -25,7 +25,8 @@ export class AuthService {
     gapi.load('client:auth2', () => {
       gapi.client.init({
         clientId: this.clientId,
-        scope: this.scope
+        scope: this.scope,
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
       }).then(() => {
         const authInstance = gapi.auth2.getAuthInstance();
         this.isSignedIn = authInstance.isSignedIn.get();
