f801010a8c078a95342425e125dfcde45f557c46
Tran Tuan Anh <trananh0604@gmail.com>
2025-06-17 10:09:24 +0700

    n

--- src/app/pages/work-order-status/work-order-status.component.ts
+++ src/app/pages/work-order-status/work-order-status.component.ts
@@ -60,7 +60,7 @@ export class WorkOrderStatusComponent implements OnInit {
 
   fetchWorkOrders() {
     this.isLoading = true;
-    this.sheetService.getSheet('W.O Mass!A3:X10').subscribe({
+    this.sheetService.getSheet('WO!A3:X10').subscribe({
       next: (res: any) => {
         const headers = res.values[0];
         this.tableHeaders = headers;
